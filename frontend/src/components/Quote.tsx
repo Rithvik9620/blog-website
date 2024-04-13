fe8bf93bcef43f6ff761a3d94a6350b0741e58bf
Rithvik <rithvikgoud7@gmail.com>
2024-04-13 11:51:40 +0530

    Changed Quote.tsx

--- frontend/src/components/Quote.tsx
+++ frontend/src/components/Quote.tsx
@@ -2,7 +2,7 @@ const Quote = () => {
   return (
     <div className="bg-slate-200 h-screen flex justify-center flex-col">
         <div className="text-3xl font-bold mx-12">
-            "The customer support I received was exceptional. The support team went above and beyond to address my concerns."
+        "Empower Your Words: Unleash Your Voice, Share Your Story, and Inspire the World."
         </div>
         <div className="mx-12 mt-4 text-xl font-semibold">
             Jules Winnfield
